15f2a8af14870ea0d8d5e9aa967dd000f64c2425
Samigold34 <samigold34@gmail.com>
2025-03-21 00:06:38 +0100

    Add files via upload

---
+++ assets/js/myscript.js
@@ -0,0 +1,139 @@
+//customers in a bank
+//let bvn = '546367389299'
+
+/*console.log(bvn);
+
+document.getElementById('bvn').innerHTML = bvn;
+
+let cusName = 'Samson'
+console.log(cusName);
+
+document.getElementById('cusName').innerHTML = cusName;
+
+let firstName = prompt("what is first name")
+
+let deleteItem = confirm("Are you sure");
+
+if (deleteItem) {
+alert("items deleted!");
+}else{
+  alert("Deletion aborted!")} */
+
+
+/*let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+console.log(a+b);
+console.log(c+d);
+
+alert(a+b); //3
+
+alert(c+d);*/ // 7
+
+
+//alert("my first name is " + firstName);
+
+//Arithmetic operators
+/* price1 = 1000;
+let price2 = 100000;
+let profit = price1 + price2;
+
+let investment = 1000000;
+let sales = 1500000;
+let profits = sales -investment;
+
+console.log("Profit is", profits);
+
+alert(profit);*/
+
+//arithmetic .class exercise
+/*let customer ="John Doe";
+let price_per_litre = 880;
+
+
+console.log(customer + " bought " + price_per_litre +'NGN');
+document.getElementById("price").innerHTML = price_per_litre + 'NGN';*/
+
+//Substraction operator
+let num1 = 300;
+let num2 = 400;
+let num3 = 500;
+
+let diff = num1 - num2;
+console.log(diff);
+document.getElementById("diff").innerHTML = diff
+
+//multiplication
+let prod = num1 * 0.5;
+console.log(prod)
+document.getElementById("prod").innerHTML = prod
+
+//division
+let r_of_div = num2/4;
+console.log(r_of_div)
+document.getElementById("r_of_div").innerHTML = r_of_div
+
+//modulus
+let mod =num2 % 2;
+console.log(mod);
+document.getElementById("mod").innerHTML = mod
+
+//exponentation
+let x = 100;
+let pow = x ** 3;
+console.log(pow);
+document.getElementById("pow").innerHTML = pow
+
+//increament
+let traffic = 10;
+console.log(traffic++);
+document.getElementById("traffic").innerHTML = traffic++
+
+//let a = 2;
+//console.log(++a)
+
+let a = 2;
+a = a + 1;
+console.log(++a);
+document.getElementById("a").innerHTML = ++a;
+
+let sum = 9;
+console.log(sum);
+document.getElementById("sum").innerHTML = sum;
+
+console.log(++sum);
+
+
+console.log((sum += 2));
+
+//console.log(++a);
+
+console.log(--sum);
+console.log(sum -= 14)
+
+let bal = 400000
+console.log(bal - bal * 0.1);
+document.getElementById("bal").innerHTML = bal - bal * 0.1;
+
+let debit = bal - 4000;
+let message = "hi"
+console.log(debit);
+document.getElementById("debit").innerHTML = bal - 4000
+//types of variable
+console.log(typeof debit)
+document.getElementById("typeof debit").innerHTML = typeof debit
+
+console.log(typeof message)
+document.getElementById("typeof message").innerHTML = typeof message 
+
+let b = false;
+console.log(typeof b)
+document.getElementById("b").innerHTML = typeof b
+console.log()
+
+
+
+
+
+
